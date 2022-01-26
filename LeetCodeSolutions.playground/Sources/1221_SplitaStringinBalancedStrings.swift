b80be9580ca4658c485bf45fb1e8f6c09e731535
Kaan Gurses <>
2022-01-26 23:26:48 +0300

    1221 solved.

---
+++ LeetCodeSolutions.playground/Sources/1221_SplitaStringinBalancedStrings.swift
@@ -0,0 +1,25 @@
+// 1221. Split a String in Balanced Strings
+
+final class Solution {
+    func balancedStringSplit(_ s: String) -> Int {
+        var splitCount = 0
+        var currentChar = ""
+        var currentCount = 0
+
+        for char in s {
+            if currentChar.isEmpty {
+                currentChar = String(char)
+                currentCount = 1
+            } else if String(char) == currentChar {
+                currentCount += 1
+            } else {
+                currentCount -= 1
+            }
+            if currentCount == 0 {
+                splitCount += 1
+            }
+        }
+
+        return splitCount
+    }
+}
