706df422ec26aa7f8a38ff2b866e4f09c4876b7b
Kaan Gurses <>
2022-04-24 23:44:53 +0200

    35 updated with the binary search

--- LeetCodeSolutions.playground/Sources/35_SearchInsertPosition.swift
+++ LeetCodeSolutions.playground/Sources/35_SearchInsertPosition.swift
@@ -1,12 +1,19 @@
+// 35. Search Insert Position
+
 final class Solution35 {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var result = 0
-        for num in nums {
-            if num >= target {
-                return result
+        var lowerBound = 0
+        var upperBound = nums.count-1
+        while lowerBound <= upperBound {
+            let mid = lowerBound + (upperBound-lowerBound)/2
+            if nums[mid] < target {
+                lowerBound = mid + 1
+            } else if nums[mid] > target {
+                upperBound = mid - 1
+            } else {
+                return mid
             }
-            result += 1
         }
-        return result
+        return lowerBound
     }
 }
