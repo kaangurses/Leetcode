0c8241ca180a678a4d362f0b84d343df1d78dfdf
Kaan Gurses <>
2022-05-31 22:11:06 +0200

    206 solved.

---
+++ LeetCodeSolutions.playground/Sources/206_ReverseLinkedList.swift
@@ -0,0 +1,25 @@
+// 206. Reverse Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+final class Solution206 {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var currentNode: ListNode? = nil
+        var tempNode = head
+        while tempNode != nil {
+            if let temp = tempNode {
+                currentNode = ListNode(temp.val, currentNode)
+            }
+            tempNode = tempNode?.next
+        }
+        return currentNode
+    }
+}
