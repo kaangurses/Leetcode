11c12856605a8a873a6231cb5a4ddb06d748e002
Kaan Gurses <>
2022-06-03 17:47:30 +0200

    46 solved.

---
+++ LeetCodeSolutions.playground/Sources/46_Permutations.swift
@@ -0,0 +1,26 @@
+// 46. Permutations
+
+final class Solution46 {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var subList: [Int] = []
+        var result: [[Int]] = []
+        backtracking(nums, &subList, &result)
+        return result
+    }
+
+    private func backtracking(_ nums: [Int], _ array: inout [Int], _ result: inout [[Int]]) {
+        if array.count == nums.count {
+            result.append(array)
+            return
+        }
+
+        for idx in 0..<nums.count {
+            if !array.contains(nums[idx]) {
+                array.append(nums[idx])
+                backtracking(nums, &array, &result)
+                array.removeLast()
+            }
+        }
+
+    }
+}
