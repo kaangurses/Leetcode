ed229e24436713d5de00bb77c17a01512366c433
Kaan Gurses <>
2022-06-04 15:33:33 +0200

    2236 solved.

---
+++ LeetCodeSolutions.playground/Sources/2236_RootEqualsSumofChildren.swift
@@ -0,0 +1,22 @@
+// 2236. Root Equals Sum of Children
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+final class Solution2236 {
+    func checkTree(_ root: TreeNode?) -> Bool {
+      (root?.val ?? 0) == (root?.left?.val ?? 0) + (root?.right?.val ?? 0)
+    }
+}
