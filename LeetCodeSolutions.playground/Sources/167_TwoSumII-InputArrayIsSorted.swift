97a61fbf3dfc92c54615bae05242f7b2d5593e65
Kaan Gurses <>
2022-06-09 09:58:00 +0200

    167 updated.

--- LeetCodeSolutions.playground/Sources/167_TwoSumII-InputArrayIsSorted.swift
+++ LeetCodeSolutions.playground/Sources/167_TwoSumII-InputArrayIsSorted.swift
@@ -2,6 +2,27 @@
 
 final class Solution167 {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var first = 0
+        var second = numbers.count-1
+        while first<second {
+            let number = numbers[first]+numbers[second]
+            if number == target {
+                return [first+1, second+1]
+            }
+            if number>target {
+                second -= 1
+            } else {
+                first += 1
+            }
+        }
+
+        return [-1, -1]
+    }
+}
+
+/*
+final class Solution167 {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
         for idx in 0..<numbers.count {
             let val = numbers[idx]
@@ -14,3 +35,18 @@ final class Solution167 {
         return [-1, -1]
     }
 }
+
+final class Solution167 {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        for i in 0..<numbers.count-1 {
+            for j in i+1..<numbers.count {
+                if numbers[i]+numbers[j] == target {
+                    return [i+1, j+1]
+                }
+            }
+        }
+
+        return [-1, -1]
+    }
+}
+*/
